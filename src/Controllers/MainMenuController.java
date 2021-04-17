eaf3d15a75b4cc2a3d565a1316ae6e4c9c0c1863
Bastian <bastian.prado.v@gmail.com>
2021-04-17 15:46:33 -0700

    Añadido botón para salir al menu de log in

--- src/Controllers/MainMenuController.java
+++ src/Controllers/MainMenuController.java
@@ -1,5 +1,22 @@
 package Controllers;
 
+import Tools.Window;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class MainMenuController {
 
+    private final Window wn = new Window();
+    @FXML
+    private AnchorPane mainMenuPane;
+
+    public void salirOnAction() {
+        try {
+            wn.changeStage(mainMenuPane,"/GUI/LogInScreen.fxml");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
